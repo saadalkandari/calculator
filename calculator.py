460840d62858b87d11a6d8171c6b77b8530ec5b4
SaadAlkandari <mr.s.alkandari@gmail.com>
2022-04-08 19:40:52 +0300

    done

--- calculator.py
+++ calculator.py
@@ -1,6 +1,32 @@
 
 def main():
 	#write your code here
+	firstN= input("Enter first number: ")
+	secondN= input("Enter second number: ")
+	operation= input("Enter the operation (+,-,*,/): ")
+
+	if firstN.isdigit() == False :
+		print("Valid Input")
+	elif secondN.isdigit() == False :
+		print("Valid Input")
+
+	if operation == "+" :
+		answer = int(firstN) + int(secondN)
+		print(answer)
+	elif operation == "-" :
+		answer = int(firstN) - int(secondN)
+		print(answer)
+	elif operation == "*" :
+		answer = int(firstN) * int(secondN)
+		print(answer)
+	elif operation == "/" :
+		answer = int(firstN) / int(secondN)
+		print(answer)
+	else:
+		print("Valid Operation")
+
+
+
 	pass
 	
 
